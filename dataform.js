65c7e8a772a35eb5a8fbd52947db711e484d3c7f
Deepak Mishra <deepak.mishra@metamug.com>
2017-06-16 21:12:38 +0530

    added data form js

---
+++ dataform.js
@@ -0,0 +1,27 @@
+function DataForm(form){
+	this.form = form
+}
+
+DataForm.prototype.fill = function(obj) {
+		
+	for (var key in obj) {
+	  if (obj.hasOwnProperty(key)) {
+
+	    var el = this.form.querySelector('[name="'+key+'"]');
+		if (el) { //only if form element is found
+			el.value = obj[key];	
+		}
+	  }
+	}
+};
+
+DataForm.prototype.load = function(url) {
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', url);
+	var that = this;
+	xhr.addEventListener('load',function(){
+		var data = JSON.parse(xhr.responseText)
+		that.fill(data[0]);
+	})
+	xhr.send();
+};
