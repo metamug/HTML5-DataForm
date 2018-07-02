95becb50d5219f4a09d44176c9989c9acbbbc4b9
Deepak Mishra <d3ep4k@users.noreply.github.com>
2018-07-03 00:00:19 +0530

    Update dataform.js

--- dataform.js
+++ dataform.js
@@ -1,13 +1,17 @@
-function DataForm(form){
-	this.form = form
+//takes form object and resource location
+function DataForm(form, url){
+	this.form = form;
+	this.resource = url;
 }
 
+
+// Fill the form with values.
 DataForm.prototype.fill = function(obj) {
 		
 	for (var key in obj) {
 	  if (obj.hasOwnProperty(key)) {
 
-	    var el = this.form.querySelector('[name="'+key+'"]');
+	    	var el = this.form.querySelector('[name="'+key+'"]');
 		if (el) { //only if form element is found
 			el.value = obj[key];	
 		}
@@ -15,9 +19,10 @@ DataForm.prototype.fill = function(obj) {
 	}
 };
 
-DataForm.prototype.load = function(url) {
+//Load Internally uses fill(obj), to load data into the form from URL
+DataForm.prototype.load = function() {
 	var xhr = new XMLHttpRequest();
-	xhr.open('GET', url);
+	xhr.open('GET', this.resource);
 	var that = this;
 	xhr.addEventListener('load',function(){
 		var data = JSON.parse(xhr.responseText)
@@ -26,12 +31,29 @@ DataForm.prototype.load = function(url) {
 	xhr.send();
 };
 
+DataForm.prototype.fillDropDown = function(){
+	var dropdowns = this.form.form.getElementsByTagName("select");
+	
+	dropdowns.forEach(function(item){
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', this.resource+"?select="+item.name)
+		xhr.addEventListener('load',function(){
+			var data = JSON.parse(xhr.responseText)
+		    data.forEach(function(option){
+				var option = document.createElement("option");
+				option.value = option.value;
+				option.text = option.text;
+			})
+		})
+		xhr.send();
+	})
+}
 
-DataForm.prototype.send = function(url) {
+DataForm.prototype.send = function() {
 	var xhr = new XMLHttpRequest();
 	var data = new FormData(this.form);
 	//open the request
-	xhr.open('POST', url)
+	xhr.open('POST', this.resource)
 	//send the form data
 	xhr.send(data);	
 };
